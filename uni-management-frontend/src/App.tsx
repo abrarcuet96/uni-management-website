e2abd0655ea26871422e089d300abcdfee6aff1b
abrarcuet96 <abrarhaider1357@gmail.com>
2025-09-05 15:00:34 +0600

    <working on dashboard main layout>

--- uni-management-frontend/src/App.tsx
+++ uni-management-frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "./components/ui/button";
+import {Button} from "./components/ui/button";
 
 function App() {
-  return (
-    <div>
-      <Button>Submit</Button>
-    </div>
-  );
+    return (
+        <div>
+            <Button>Submit</Button>
+        </div>
+    );
 }
 
 export default App;
